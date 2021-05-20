661e62f9c8ceda1596331846a2e47ba7e2db0ea8
Arthur <Arthur@G5-5505Arthur>
2021-05-20 11:56:08 +0200

    0 pour skip

--- MiniProjet/Game/Main/Launch.java
+++ MiniProjet/Game/Main/Launch.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 
 import Run.AePlayWave;
 import Run.RunGame;
+import Run.Update;
 public class Launch {
+
 	public static void main (String[] arg){
 	    //music INTRO
 		AePlayWave Intro = new AePlayWave(System.getProperty("user.dir") + "\\Audio\\Title_Screen01.wav");
 		Intro.start();
+		Update skip = new Update();
+		skip.setDaemon(true);
+		skip.start();
 		System.out.println("Bonjour à toi jeune aventurier. Par manque d'argent, par recherche de succès ou par goût du combat, tu entres aujourd'hui dans l'arène.");
-		pause(3000);
+		pause(skip.val);
 		System.out.println("En plein coeur de la capitale, l'arène du Lion est un diamant planté au beau milieu de la Cité brillant de mille feux.");
-		pause(3000);
+		pause(skip.val);
 		System.out.println("Les arcades de ce somptueux bâtiment ont vu passer les meilleurs musiciens, acteurs, mais aussi des animaux, des spectacles de cirque, des jeux sportifs.");
-		pause(3000);
+		pause(skip.val);
 		System.out.println("Mais, l'attraction phare du stade demeure les combats de gladiateurs. La crème de la cité se bouscule pour observer les combats.");
-		pause(3000);
+		pause(skip.val);
 		System.out.println("Pour les gladiateurs, c'est une opportunité de gagner beaucoup d'argent, de devenir célèbre, ou d'améliorer ses aptitudes au combat.");
-		pause(3000);
+		pause(skip.val);
 		System.out.println("Que tu sois mage, guerrier, assassin, archer, tous les coups sont permis. L'issue ne peut être que la victoire ou la mort.");
-		pause(3000);
+		pause(skip.val);
 		System.out.println("Vous entrez donc d'un pas sûr sur le terrain, confiant en vos aptitudes guerrières.");	
-		pause(3000);
+		pause(2000);
 		for(int i=0;i<50;i++) {System.out.println();}	
 		
 		System.out.println("Combien de joueurs êtes-vous ?");
---
+++ MiniProjet/Game/Run/Update.java
@@ -0,0 +1,22 @@
+package Run;
+import java.util.Scanner;
+
+
+public class Update extends Thread {
+	public int val=3000;
+
+
+	public void run(){
+		Scanner stop = new Scanner(System.in);
+		while(true) {
+
+			int tmp = stop.nextInt();
+			if (tmp==0) {
+			this.val = 0;
+
+			}
+		}
+	}
+
+
+}
